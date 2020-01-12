eb4dee95154352d9ad5dcf89911c390291c42555
KKH <nepho@blocko.io>
2020-01-12 20:29:40 +0900

    fountain code

---
+++ fountain.lua
@@ -0,0 +1,24 @@
+state.var {
+  GData = state.map(),
+  GIdx = state.value(),
+  GEntIdx = state.value(),
+  GAmount = state.value()
+}
+
+function constructor()
+  GameIdx:set(1)
+  GameEntIdx:set(1)
+  GAmount:set(1 * 1000000000000000000)
+end
+
+function default()
+  assert(system.getAmount() == GAmount:get(), "Your amount was " .. system.getAmount() .. ". Only " .. GAmount:get() .. " AER can participate.")
+
+  GData[GIdx:get()][GEntIdx:get()] = {
+    sender = system.getSender(),
+    amount = system.getAmount(),
+    txid = system.getTxhash()
+  }
+
+  GEntIdx:set(GEntIdx:get() + 1)
+end
